89df3bf01b1f8f5a1050285bfb0f4cd25659ab84
similadayo <similoluwailesanm@outlook.com>
2023-12-10 20:21:35 +0100

    Handleconnection function to upgrade http connection to websocket

--- websocket/websocket.go
+++ websocket/websocket.go
@@ -24,3 +24,62 @@ type Message struct {
 	sender  string `json:"sender"`
 	content string `json:"content"`
 }
+
+// Hanldeconnection upgrades the http connection to a websocket connection
+func HandleConnection(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	params := r.URL.Query()
+	userName := params.Get("userName")
+	roomName := params.Get("room")
+
+	client := &Client{
+		conn:     ws,
+		userName: userName,
+		Room:     Chat.GetRoom(roomName),
+	}
+
+	client.Room.Register <- client
+
+	go client.Read()
+	go client.Write()
+}
+
+// Read reads messages from the websocket connection
+func (c *Client) Read() {
+	defer func() {
+		c.Room.Unregister <- c
+		c.conn.Close()
+	}()
+
+	for {
+		_, msg, err := c.conn.ReadMessage()
+		if err != nil {
+			panic(err)
+		}
+
+		c.Room.Broadcast <- Message{sender: c.userName, content: string(msg)}
+	}
+}
+
+// Write writes messages to the websocket connection
+func (c *Client) Write() {
+	defer func() {
+		c.conn.Close()
+	}()
+
+	for {
+		select {
+		case msg, ok := <-c.Room.Messages:
+			if !ok {
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			c.conn.WriteMessage(websocket.TextMessage, []byte(msg))
+		}
+	}
+}
